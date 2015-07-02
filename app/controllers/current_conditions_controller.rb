2b26582dcf6c83d58af5b94abbbd63924ebec879
Eric Oligney <EOengineer@gmail.com>
2015-07-01 23:01:08 -0400

    changed model to return a hash with various attributes.

--- app/controllers/current_conditions_controller.rb
+++ app/controllers/current_conditions_controller.rb
@@ -1,9 +1,9 @@
 class CurrentConditionsController < ApplicationController
   def index
     if params[:current_condition]
-      @current_conditions = CurrentCondition.new(params[:current_condition][:zip_code])
+      @current_conditions = CurrentCondition.new(params[:current_condition][:zip_code]).gather_all
     else
-      @current_conditions = CurrentCondition.new
+      @current_conditions = CurrentCondition.new.gather_all
     end
   end
 end
--- app/models/current_condition.rb
+++ app/models/current_condition.rb
@@ -1,84 +1,31 @@
 class CurrentCondition
   attr_reader :zip_code
 
-  def initialize(zip_code = "34102")
-    @zip_code = zip_code
+  def initialize(zip_code)
+    @zip_code = zip_code.present? ? zip_code : "34102"
   end
 
-  def city_state
-    current_weather["display_location"]["full"]
-  end
-
-  def last_updated
-    current_weather["observation_time"]
-  end
-
-  def local_time
-    current_weather["local_time_rfc822"]
-  end
-
-  def overview_name
-    current_weather["weather"]
-  end
-
-  def icon
-    current_weather["icon_url"]
-  end
-
-  def temp_f
-    current_weather["temp_f"]
-  end
-
-  def temp_c
-    current_weather["temp_c"]
-  end
-
-  def relative_humidity
-    current_weather["relative_humidity"]
-  end
-
-  def wind_intensity
-    current_weather["wind_string"]
-  end
-
-  def wind_direction
-    current_weather["wind_dir"]
-  end
-
-  def wind_speed
-    current_weather["wind_mph"]
-  end
-
-  def wind_gust_speed
-    current_weather["wind_gust_mph"]
-  end
-
-  def barometric_pressure
-    current_weather["pressure_in"]
-  end
-
-  def dewpoint
-    current_weather["dewpoint_f"]
-  end
-
-  def feels_like
-    current_weather["feelslike_f"]
-  end
-
-  def visibility
-    current_weather["visibility_m"]
-  end
-
-  def uv_index
-    current_weather["UV"]
-  end
-
-  def precip_today
-    current_weather["precip_today_in"]
-  end
-
-  def hour_precip
-    current_weather["precip_1hr_in"]
+  def gather_all
+    { city_state: current_weather["display_location"]["full"],
+      last_updated: current_weather["observation_time"],
+      local_time: current_weather["local_time_rfc822"],
+      overview_name: current_weather["weather"],
+      icon: current_weather["icon_url"],
+      temp_f: current_weather["temp_f"],
+      temp_c: current_weather["temp_c"],
+      relative_humidity: current_weather["relative_humidity"],
+      wind_intensity: current_weather["wind_string"],
+      wind_direction: current_weather["wind_dir"],
+      wind_speed: current_weather["wind_mph"],
+      wind_gust_speed: current_weather["wind_gust_mph"],
+      barometric_pressure: current_weather["pressure_in"],
+      dewpoint: current_weather["dewpoint_f"],
+      feels_like: current_weather["feelslike_f"],
+      visibility: current_weather["visibility_m"],
+      uv_index: current_weather["UV"],
+      precip_today: current_weather["precip_today_in"],
+      hour_precip: current_weather["precip_1hr_in"]
+    }
   end
 
 
@@ -90,4 +37,5 @@ class CurrentCondition
   def current_weather
     current_weather ||= w_api.conditions_for(@zip_code)["current_observation"]
   end
+
 end
